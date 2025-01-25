b469cb58b1984f6dda6c67a01e222d87e3d16f29
Chris Vaillancourt <cvaillan22@gmail.com>
2025-01-25 13:51:31 -0800

    build: add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,19 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [TanStackRouterVite(), react()],
+	plugins: [
+		TanStackRouterVite(),
+		react({
+			babel: {
+				plugins: [
+					[
+						'babel-plugin-react-compiler',
+						{
+							target: '19',
+						},
+					],
+				],
+			},
+		}),
+	],
 });

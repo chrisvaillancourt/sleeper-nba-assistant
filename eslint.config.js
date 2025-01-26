a0a6fae6395cff44a400860e1400ea064da90914
Chris Vaillancourt <cvaillan22@gmail.com>
2025-01-25 16:41:57 -0800

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,60 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import reactCompiler from 'eslint-plugin-react-compiler';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 
 export default tseslint.config(
-	{ ignores: ['dist'] },
+	eslint.configs.recommended,
+	tseslint.configs.strict,
+	tseslint.configs.stylistic,
+	reactPlugin.configs.flat.recommended,
+	reactPlugin.configs.flat['jsx-runtime'],
 	{
-		extends: [js.configs.recommended, ...tseslint.configs.recommended],
-		files: ['**/*.{ts,tsx}'],
+		settings: {
+			// had to be in a separate object to fix cli warning
+			react: {
+				version: 'detect',
+			},
+		},
+	},
+	{
+		ignores: ['dist'],
+		files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+		...jsxA11yPlugin.flatConfigs.recommended,
 		languageOptions: {
+			...jsxA11yPlugin.flatConfigs.recommended.languageOptions,
+			...reactPlugin.configs.flat.recommended.languageOptions,
 			ecmaVersion: 2020,
 			globals: globals.browser,
 		},
 		plugins: {
-			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh,
 			'react-compiler': reactCompiler,
 		},
 		rules: {
-			...reactHooks.configs.recommended.rules,
+			'react/jsx-uses-react': 'off',
+			'react/react-in-jsx-scope': 'off',
 			'react-refresh/only-export-components': [
 				'warn',
 				{ allowConstantExport: true },
 			],
 			'react-compiler/react-compiler': 'error',
 		},
+	},
+	// hack, can remove after https://github.com/facebook/react/issues/28313 is resolved
+	// https://github.com/facebook/react/pull/30774#issuecomment-2599121764
+	{
+		plugins: {
+			// @ts-expect-error see above comment about related issue
+			'react-hooks': reactHooksPlugin,
+		},
+		// @ts-expect-error see above comment about related issue
+		rules: { ...reactHooksPlugin.configs.recommended.rules },
 	},
 	eslintConfigPrettier,
 );
